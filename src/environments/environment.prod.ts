49c8bcfa6a7ed344da7d6647eae923c4fd509553
prerna <prernathanai@gmail.com>
2025-05-31 18:39:03 +0530

    connect url updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net',
-    registerUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/register',
-    loginUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/login',
-    refreshTokenUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/refresh',
-    userUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/members/current',
-    allUsersUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/members/',
-    userApiUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/members',
-    logoutUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/logout',
+    apiUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net',
+    registerUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net/api/auth/register',
+    loginUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net/api/auth/login',
+    refreshTokenUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net/api/auth/refresh',
+    userUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net/api/members/current',
+    allUsersUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net/api/members/',
+    userApiUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net/api/members',
+    logoutUrl: 'https://k-backend-ebe6gzhbf2dfgjca.centralus-01.azurewebsites.net/api/auth/logout',
     phoneapiKey: ''
   };
