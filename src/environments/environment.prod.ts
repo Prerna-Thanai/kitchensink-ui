7f067d9ef31d88c64330e1fcdc7a403d7f08ec4d
prerna <prernathanai@gmail.com>
2025-05-31 16:49:24 +0530

    deployment related changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net'
+    apiUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net',
+    registerUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/register',
+    loginUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/login',
+    refreshTokenUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/refresh',
+    userUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/members/current',
+    allUsersUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/members/',
+    userApiUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/members',
+    logoutUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api/auth/logout',
+    phoneapiKey: ''
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
     registerUrl: 'http://localhost:8080/api/auth/register',
     loginUrl: 'http://localhost:8080/api/auth/login',
     refreshTokenUrl: 'http://localhost:8080/api/auth/refresh',
-    userUrl: 'http://localhost:8080/api/member/current',
-    allUsersUrl: 'http://localhost:8080/api/member/all',
-    userApiUrl: 'http://localhost:8080/api/member',
+    userUrl: 'http://localhost:8080/api/members/current',
+    allUsersUrl: 'http://localhost:8080/api/members/',
+    userApiUrl: 'http://localhost:8080/api/members',
     logoutUrl: 'http://localhost:8080/api/auth/logout',
     phoneapiKey: ''
 
