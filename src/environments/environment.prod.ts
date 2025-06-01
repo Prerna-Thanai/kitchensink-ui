e12ac5da5d796ceca0e7a5f3515cccfaa8220a45
prerna <prernathanai@gmail.com>
2025-06-01 17:47:18 +0530

    updated azure cred

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net',
-    registerUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net/api/auth/register',
-    loginUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net/api/auth/login',
-    refreshTokenUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net/api/auth/refresh',
-    userUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net/api/members/current',
-    allUsersUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net/api/members/',
-    userApiUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net/api/members',
-    logoutUrl: 'https://kitchensink-be-dhc5bphchpgda5b6.southeastasia-01.azurewebsites.net/api/auth/logout',
+    apiUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net/',
+    registerUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net//api/auth/register',
+    loginUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net//api/auth/login',
+    refreshTokenUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net//api/auth/refresh',
+    userUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net//api/members/current',
+    allUsersUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net//api/members/',
+    userApiUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net//api/members',
+    logoutUrl: 'https://kitchensink-be-dpccd2abfscyeqhq.centralindia-01.azurewebsites.net//api/auth/logout',
     phoneapiKey: ''
   };
